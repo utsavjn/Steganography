1be6b8bbfdbb6b8260051e509ab161e92c745045
Utsav Jain <utsavj77@gmail.com>
2016-02-08 21:47:40 -0800

    Steganography
    This application lets you embed or hide important or private messages or
    files into picture  files like JPG, GIF and BMP Bitmap images without
    affecting the quality of actual images or files. It achieves this by
    using the least significant bits of these files for embedding data which
    are not used by the Image viewers or Image editors.

---
+++ MyJMenuItem.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication1;
+
+/**
+ *
+ * @author Utsav
+ */
+class MyJMenuItem {
+
+    public MyJMenuItem(String string, int i, char c) {
+    }
+
+}
